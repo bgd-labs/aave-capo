bfedc966bbc8d222b7b162b2e2eccaad76e948d0
Rustem Kurmaev <rustboyar@gmail.com>
2024-04-17 14:54:13 +0200

    fix isCapped test

--- tests/AaveV2/BaseTestV2.sol
+++ tests/AaveV2/BaseTestV2.sol
@@ -6,6 +6,7 @@ import 'forge-std/Test.sol';
 import {IACLManager, BasicIACLManager} from 'aave-address-book/AaveV3.sol';
 import {GovV3Helpers} from 'aave-helpers/GovV3Helpers.sol';
 import {IPriceCapAdapterStable, ICLSynchronicityPriceAdapter} from '../../src/interfaces/IPriceCapAdapterStable.sol';
+import {ICLSynchronicityPriceAdapter} from 'cl-synchronicity-price-adapter/interfaces/ICLSynchronicityPriceAdapter.sol';
 
 abstract contract BaseTestV2 is Test {
   struct ForkParams {
@@ -49,19 +50,21 @@ abstract contract BaseTestV2 is Test {
 
   function test_latestAnswerRel() public virtual {
     for (uint i = 0; i < adapterParams.preRequisiteAdapters.length; i++) {
-      GovV3Helpers.deployDeterministic(adapterParams.preRequisiteAdapters[i]);
+      address preAdapter = GovV3Helpers.deployDeterministic(adapterParams.preRequisiteAdapters[i]);
+
+      if (i == adapterParams.preRequisiteAdapters.length - 1) {
+        bool isCapped = IPriceCapAdapterStable(preAdapter).isCapped();
+        assertFalse(isCapped);
+      }
     }
 
-    IPriceCapAdapterStable adapter = IPriceCapAdapterStable(
+    ICLSynchronicityPriceAdapter adapter = ICLSynchronicityPriceAdapter(
       GovV3Helpers.deployDeterministic(adapterParams.adapterCode)
     );
 
     int256 price = adapter.latestAnswer();
     int256 priceOfNotCappedAdapter = NOT_CAPPED_ADAPTER.latestAnswer();
 
-    bool isPriceCapped = adapter.isCapped();
-
-    assertEq(isPriceCapped, false);
     assertApproxEqRel(price, priceOfNotCappedAdapter, 2 * 1e16);
   }
 
@@ -78,16 +81,13 @@ abstract contract BaseTestV2 is Test {
         GovV3Helpers.deployDeterministic(adapterParams.preRequisiteAdapters[i]);
       }
 
-      IPriceCapAdapterStable adapter = IPriceCapAdapterStable(
+      ICLSynchronicityPriceAdapter adapter = ICLSynchronicityPriceAdapter(
         GovV3Helpers.deployDeterministic(adapterParams.adapterCode)
       );
 
       int256 price = adapter.latestAnswer();
       int256 priceOfNotCappedAdapter = NOT_CAPPED_ADAPTER.latestAnswer();
 
-      bool isPriceCapped = adapter.isCapped();
-
-      assertEq(isPriceCapped, false);
       assertApproxEqRel(price, priceOfNotCappedAdapter, 2 * 1e16);
 
       currentBlock += retrospectionParams.step;
