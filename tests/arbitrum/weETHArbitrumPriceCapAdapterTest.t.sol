53a16ed7c937ff876eec10a105ee796955b118a1
Rustem Kurmaev <rustboyar@gmail.com>
2024-04-22 19:36:14 +0200

    major tests rework

--- tests/arbitrum/weETHArbitrumPriceCapAdapterTest.t.sol
+++ tests/arbitrum/weETHArbitrumPriceCapAdapterTest.t.sol
@@ -11,55 +11,10 @@ import {CapAdaptersCodeArbitrum} from '../../scripts/DeployArbitrumWeEth.s.sol';
 contract weETHArbitrumPriceCapAdapterTest is CLAdapterBaseTest {
   constructor()
     CLAdapterBaseTest(
-      address(0),
       CapAdaptersCodeArbitrum.weETHAdapterCode(),
-      ForkParams({network: 'arbitrum', blockNumber: 197799635}),
-      RetrospectionParams({
-        maxYearlyRatioGrowthPercent: 8_75,
-        minimumSnapshotDelay: 7 days,
-        startBlock: 184050978,
-        finishBlock: 197799635,
-        delayInBlocks: 2550000, // > 7 days
-        step: 1000000
-      }),
-      CapParams({maxYearlyRatioGrowthPercent: 2_00, startBlock: 184050978, finishBlock: 197799635}),
-      AdapterCreationDefaultParams({
-        aclManager: AaveV3Arbitrum.ACL_MANAGER,
-        baseAggregatorAddress: AaveV3ArbitrumAssets.WETH_ORACLE,
-        ratioProviderAddress: CapAdaptersCodeArbitrum.weETH_eETH_AGGREGATOR,
-        pairDescription: 'Capped weETH / eETH(ETH) / USD'
-      })
+      AaveV3ArbitrumAssets.WETH_ORACLE,
+      10,
+      ForkParams({network: 'arbitrum', blockNumber: 197799635})
     )
   {}
-
-  function test_latestAnswer() public override {}
-
-  function test_latestAnswerRetrospective() public override {}
-
-  function test_cappedLatestAnswer() public override {
-    vm.createSelectFork(vm.rpcUrl(forkParams.network), capParams.startBlock);
-
-    // create adapter with initial parameters
-    IPriceCapAdapter adapter = createAdapterSimple(
-      7 days,
-      uint40(block.timestamp - 8 days),
-      capParams.maxYearlyRatioGrowthPercent
-    );
-
-    skip(1);
-
-    // persist adapter
-    vm.makePersistent(address(adapter));
-
-    // roll fork to the finish block
-    vm.createSelectFork(vm.rpcUrl(forkParams.network), capParams.finishBlock);
-
-    bool isCapped = adapter.isCapped();
-
-    // compare prices
-    assertTrue(isCapped, 'price is not capped');
-
-    vm.revokePersistent(address(adapter));
-    vm.createSelectFork(vm.rpcUrl(forkParams.network), capParams.finishBlock);
-  }
 }
--- tests/ethereum/weETHPriceCapAdapterTest.t.sol
+++ tests/ethereum/weETHPriceCapAdapterTest.t.sol
@@ -11,91 +11,24 @@ import {CapAdaptersCodeEthereum} from '../../scripts/DeployEthereumWeEth.s.sol';
 contract weETHPriceCapAdapterTest is BaseTest {
   constructor()
     BaseTest(
-      address(0),
       CapAdaptersCodeEthereum.weETHAdapterCode(),
-      ForkParams({network: 'mainnet', blockNumber: 19575450}),
-      RetrospectionParams({
-        maxYearlyRatioGrowthPercent: 8_75,
-        minimumSnapshotDelay: 7 days,
-        startBlock: 18061286,
-        finishBlock: 19368742,
-        delayInBlocks: 50200,
-        step: 20000
-      }),
-      CapParams({maxYearlyRatioGrowthPercent: 2_00, startBlock: 19183379, finishBlock: 19575450})
+      AaveV3EthereumAssets.WETH_ORACLE,
+      90,
+      ForkParams({network: 'mainnet', blockNumber: 19575450})
     )
   {}
 
-  function createAdapter(
-    IACLManager aclManager,
-    address baseAggregatorAddress,
-    address ratioProviderAddress,
-    string memory pairDescription,
-    uint48 minimumSnapshotDelay,
-    IPriceCapAdapter.PriceCapUpdateParams memory priceCapParams
-  ) public override returns (IPriceCapAdapter) {
+  function _createAdapter(
+    CapAdapterParams memory capAdapterParams
+  ) internal override returns (IPriceCapAdapter) {
     return
       new WeETHPriceCapAdapter(
-        aclManager,
-        baseAggregatorAddress,
-        ratioProviderAddress,
-        pairDescription,
-        minimumSnapshotDelay,
-        priceCapParams
+        capAdapterParams.aclManager,
+        capAdapterParams.baseAggregatorAddress,
+        capAdapterParams.ratioProviderAddress,
+        capAdapterParams.pairDescription,
+        capAdapterParams.minimumSnapshotDelay,
+        capAdapterParams.priceCapParams
       );
-  }
-
-  function createAdapterSimple(
-    uint48 minimumSnapshotDelay,
-    uint104 currentRatio,
-    uint48 snapshotTimestamp,
-    uint16 maxYearlyRatioGrowthPercent
-  ) public override returns (IPriceCapAdapter) {
-    return
-      createAdapter(
-        AaveV3Ethereum.ACL_MANAGER,
-        AaveV3EthereumAssets.WETH_ORACLE,
-        CapAdaptersCodeEthereum.weETH,
-        'weETH / eETH (ETH) / USD',
-        minimumSnapshotDelay,
-        currentRatio,
-        snapshotTimestamp,
-        maxYearlyRatioGrowthPercent
-      );
-  }
-
-  function getCurrentRatio() public view override returns (uint104) {
-    return uint104(IWeEth(CapAdaptersCodeEthereum.weETH).getRate());
-  }
-
-  function test_latestAnswer() public override {}
-
-  function test_latestAnswerRetrospective() public override {}
-
-  function test_cappedLatestAnswer() public override {
-    vm.createSelectFork(vm.rpcUrl(forkParams.network), capParams.startBlock);
-
-    // create adapter with initial parameters
-    IPriceCapAdapter adapter = createAdapterSimple(
-      7 days,
-      uint40(block.timestamp - 8 days),
-      capParams.maxYearlyRatioGrowthPercent
-    );
-
-    skip(1);
-
-    // persist adapter
-    vm.makePersistent(address(adapter));
-
-    // roll fork to the finish block
-    vm.createSelectFork(vm.rpcUrl(forkParams.network), capParams.finishBlock);
-
-    bool isCapped = adapter.isCapped();
-
-    // compare prices
-    assertTrue(isCapped, 'price is not capped');
-
-    vm.revokePersistent(address(adapter));
-    vm.createSelectFork(vm.rpcUrl(forkParams.network), capParams.finishBlock);
   }
 }
