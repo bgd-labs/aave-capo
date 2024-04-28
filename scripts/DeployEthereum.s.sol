aae9ea03b7e01c972eec24561efd75baced8a42a
Rustem Kurmaev <rustboyar@gmail.com>
2024-04-28 22:47:04 +0200

    constructor params for stables

--- scripts/DeployEthereum.s.sol
+++ scripts/DeployEthereum.s.sol
@@ -4,6 +4,7 @@ import {GovV3Helpers} from 'aave-helpers/GovV3Helpers.sol';
 import {EthereumScript} from 'aave-helpers/ScriptUtils.sol';
 import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
 
+import {IPriceCapAdapterStable, IChainlinkAggregator} from '../src/interfaces/IPriceCapAdapterStable.sol';
 import {PriceCapAdapterStable} from '../src/contracts/PriceCapAdapterStable.sol';
 
 library CapAdaptersStablesCodeEthereum {
@@ -11,10 +12,12 @@ library CapAdaptersStablesCodeEthereum {
     abi.encodePacked(
       type(PriceCapAdapterStable).creationCode,
       abi.encode(
-        AaveV3Ethereum.ACL_MANAGER,
-        AaveV3EthereumAssets.USDT_ORACLE,
-        'Capped USDT/USD',
-        int256(1.04 * 1e8)
+        IPriceCapAdapterStable.CapAdapterStableParams({
+          aclManager: AaveV3Ethereum.ACL_MANAGER,
+          assetToUsdAggregator: IChainlinkAggregator(AaveV3EthereumAssets.USDT_ORACLE),
+          adapterDescription: 'Capped USDT/USD',
+          priceCap: int256(1.04 * 1e8)
+        })
       )
     );
 }
