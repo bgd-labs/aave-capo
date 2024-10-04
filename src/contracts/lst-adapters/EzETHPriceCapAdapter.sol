b3429a683b20f0ab8a9147f5037ab5494bd1da39
ianflexa <ianflexa@gmail.com>
2024-10-04 08:58:07 -0400

    docs: more description about oracle risks

--- src/contracts/lst-adapters/EzETHPriceCapAdapter.sol
+++ src/contracts/lst-adapters/EzETHPriceCapAdapter.sol
@@ -14,14 +14,17 @@ import {IEzETHRestakeManager} from '../../interfaces/IEzETH.sol';
  * @notice Price capped adapter to calculate price of (ezETH / USD) pair by using
  * @notice Chainlink data feed for (ETH / USD) and (ezETH / ETH) ratio.
  * @dev IMPORTANT: The `calculateTVLs` function called in the `RATIO_PROVIDER` is used to calculate the total assets deposited within the system
- * (operators and withdraw contract), and those type of calculations needs to use some type of virtual accounting to avoid the well-known 'donation attacks.' 
+ * (operators and Withdraw contract), and those type of calculations needs to use some type of virtual accounting to avoid the well-known 'donation attacks'.
  * In one of the calls inside the `calculateTVLs`, there is a usage of `balanceOf`(specifically in the calculation of LSTs in the Withdraw contract)
  * being subject to a donation attack. But it's understood that, exclusively in this case, the donations cannot cause a 'completed' manipulation because
- * the token donated cannot be rescued after the donation and can be considered as an 'injection of rewards', which would benefit all ezETH shareholders.
+ * the token donated cannot be rescued after the donation and can be considered as an 'injection of rewards', which would benefit all ezETH shareholders. Also,
+ * it is important to mention that the Aave Protocol does not allow the ezETH to be borrowed in its system, which limits any benefit to the attacker from the 'possible'
+ * manipulation of the exchange rate. Other systems using this oracle should consider and evaluate those risks described here.
  * More information can be found in the ezETH discussion in the forum: https://governance.aave.com/t/arfc-onboard-ezeth-to-aave-v3-lido-instance/18504/9#p-48707-asset-pricing-13
  */
 contract EzETHPriceCapAdapter is PriceCapAdapterBase {
   IERC20 internal constant ezETH = IERC20(0xbf5495Efe5DB9ce00f80364C8B423567e58d2110);
+
   /**
    * @param capAdapterParams parameters to create cap adapter
    */
