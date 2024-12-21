8d65385c124051c4923ad6fc48620f8adf5e158e
Rozengarden <github@rozengarden.eu>
2024-12-21 01:25:57 +0100

    feat: capo for ezETH on Arbitrum & Base

--- tests/arbitrum/ezETHArbitrumPriceCapAdapterTest.t.sol
+++ tests/arbitrum/ezETHArbitrumPriceCapAdapterTest.t.sol
@@ -8,13 +8,13 @@ import {IPriceCapAdapter} from '../../src/interfaces/IPriceCapAdapter.sol';
 import {CLAdapterBaseTest} from '../CLAdapterBaseTest.sol';
 import {CapAdaptersCodeArbitrum} from '../../scripts/DeployArbitrum.s.sol';
 
-contract weETHArbitrumPriceCapAdapterTest is CLAdapterBaseTest {
+contract ezETHArbitrumPriceCapAdapterTest is CLAdapterBaseTest {
   constructor()
     CLAdapterBaseTest(
-      CapAdaptersCodeArbitrum.weETHAdapterCode(),
-      30,
-      ForkParams({network: 'arbitrum', blockNumber: 197799635}),
-      'weETH_arbitrum'
+      CapAdaptersCodeArbitrum.ezETHAdapterCode(),
+      90,
+      ForkParams({network: 'arbitrum', blockNumber: 286888946}),
+      'ezETH_arbitrum'
     )
   {}
 }
