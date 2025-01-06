5dcd40273705892ab9b25127767f44a07ec5f828
Alice <121383428+Rozengarden@users.noreply.github.com>
2025-01-07 00:27:39 +0100

    Update tests/base/ezETHBasePriceCapAdapterTest.t.sol
    Co-authored-by: Ian Flexa <85500650+ianflexa@users.noreply.github.com>

--- tests/base/ezETHBasePriceCapAdapterTest.t.sol
+++ tests/base/ezETHBasePriceCapAdapterTest.t.sol
@@ -1,10 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
-import {AaveV3Base, AaveV3BaseAssets} from 'aave-address-book/AaveV3Base.sol';
-import {MiscBase} from 'aave-address-book/MiscBase.sol';
-
-import {IPriceCapAdapter} from '../../src/interfaces/IPriceCapAdapter.sol';
 import {CLAdapterBaseTest} from '../CLAdapterBaseTest.sol';
 import {CapAdaptersCodeBase} from '../../scripts/DeployBase.s.sol';
 
