cc9236a279cb54b891f09533678ee18a56508616
Alice <121383428+Rozengarden@users.noreply.github.com>
2025-01-07 00:27:52 +0100

    Update tests/arbitrum/ezETHArbitrumPriceCapAdapterTest.t.sol
    Co-authored-by: Ian Flexa <85500650+ianflexa@users.noreply.github.com>

--- tests/arbitrum/ezETHArbitrumPriceCapAdapterTest.t.sol
+++ tests/arbitrum/ezETHArbitrumPriceCapAdapterTest.t.sol
@@ -1,10 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
-import {AaveV3Arbitrum, AaveV3ArbitrumAssets} from 'aave-address-book/AaveV3Arbitrum.sol';
-import {MiscArbitrum} from 'aave-address-book/MiscArbitrum.sol';
-
-import {IPriceCapAdapter} from '../../src/interfaces/IPriceCapAdapter.sol';
 import {CLAdapterBaseTest} from '../CLAdapterBaseTest.sol';
 import {CapAdaptersCodeArbitrum} from '../../scripts/DeployArbitrum.s.sol';
 
