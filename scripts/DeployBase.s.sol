2d65383d8ebf13db845ab4bd57c59f3e9afbfef3
ianflexa <ianflexa@gmail.com>
2025-03-17 07:02:01 -0400

    chore: removing prev rsETH adapter

--- scripts/DeployBase.s.sol
+++ scripts/DeployBase.s.sol
@@ -7,8 +7,6 @@ import {AaveV3Base, AaveV3BaseAssets} from 'aave-address-book/AaveV3Base.sol';
 
 import {CLRatePriceCapAdapter, IPriceCapAdapter} from '../src/contracts/CLRatePriceCapAdapter.sol';
 
-import {RsETHL2PriceCapAdapter} from '../src/contracts/lst-adapters/RsETHL2PriceCapAdapter.sol';
-
 library CapAdaptersCodeBase {
   address public constant weETH_eETH_AGGREGATOR = 0x35e9D7001819Ea3B39Da906aE6b06A62cfe2c181;
   address public constant ezETH_ETH_AGGREGATOR = 0xC4300B7CF0646F0Fe4C5B2ACFCCC4dCA1346f5d8;
@@ -51,27 +49,6 @@ library CapAdaptersCodeBase {
               snapshotRatio: 1029613402295302804,
               snapshotTimestamp: 1733389347, // 2024-12-05
               maxYearlyRatioGrowthPercent: 10_89
-            })
-          })
-        )
-      );
-  }
-
-  function rsETHAdapterCode() internal pure returns (bytes memory) {
-    return
-      abi.encodePacked(
-        type(RsETHL2PriceCapAdapter).creationCode,
-        abi.encode(
-          IPriceCapAdapter.CapAdapterParams({
-            aclManager: AaveV3Base.ACL_MANAGER,
-            baseAggregatorAddress: AaveV3BaseAssets.WETH_ORACLE,
-            ratioProviderAddress: rsETH_LRT_ORACLE,
-            pairDescription: 'Capped rsETH / ETH / USD',
-            minimumSnapshotDelay: 14 days,
-            priceCapParams: IPriceCapAdapter.PriceCapUpdateParams({
-              snapshotRatio: 1035909659684521016,
-              snapshotTimestamp: 1738564013, // Feb-03-2025
-              maxYearlyRatioGrowthPercent: 9_83
             })
           })
         )
