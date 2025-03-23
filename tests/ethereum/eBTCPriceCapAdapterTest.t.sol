d3d6ae8a31078cb33c0fd075d239014a30f82ee6
ianflexa <ianflexa@gmail.com>
2025-03-23 13:33:59 -0400

    feat: add configuration in base test

--- tests/ethereum/eBTCPriceCapAdapterTest.t.sol
+++ tests/ethereum/eBTCPriceCapAdapterTest.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import '../BaseTest.sol';
 
 import {EBTCPriceCapAdapter} from '../../src/contracts/lst-adapters/EBTCPriceCapAdapter.sol';
+import {IEBTC} from '../../src/interfaces/IEBTC.sol';
 import {CapAdaptersCodeEthereum} from '../../scripts/DeployEthereum.s.sol';
 
 contract EBTCPriceCapAdapterTest is BaseTest {
@@ -20,5 +21,13 @@ contract EBTCPriceCapAdapterTest is BaseTest {
     IPriceCapAdapter.CapAdapterParams memory capAdapterParams
   ) internal override returns (IPriceCapAdapter) {
     return new EBTCPriceCapAdapter(capAdapterParams);
+  }
+
+  function _mockRatioProviderRate(uint256 amount) internal override {
+      vm.mockCall(
+      CapAdaptersCodeEthereum.eBTC_ACCOUNTANT,
+      abi.encodeWithSelector(IEBTC.getRate.selector),
+      abi.encode(amount)
+    );
   }
 }
