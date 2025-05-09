745947b770a3a1972ab6bc81de59bf0e0d46bb60
ianflexa <ianflexa@gmail.com>
2025-05-09 08:20:24 -0400

    chore: add more reports

--- tests/ethereum/rETHPriceCapAdapterTest.t.sol
+++ tests/ethereum/rETHPriceCapAdapterTest.t.sol
@@ -1,17 +1,19 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
-import '../BaseTest.sol';
+import '../BaseTestSvr.sol';
+import {AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
 
 import {RETHPriceCapAdapter} from '../../src/contracts/lst-adapters/RETHPriceCapAdapter.sol';
 import {CapAdaptersCodeEthereum} from '../../scripts/DeployEthereum.s.sol';
 
-contract rETHPriceCapAdapterTest is BaseTest {
+contract rETHPriceCapAdapterTest is BaseTestSvr {
   constructor()
-    BaseTest(
+    BaseTestSvr(
+      AaveV3EthereumAssets.rETH_ORACLE,
       CapAdaptersCodeEthereum.rETHAdapterCode(),
-      7,
-      ForkParams({network: 'mainnet', blockNumber: 22195655}),
+      30,
+      ForkParams({network: 'mainnet', blockNumber: 22441800}),
       'rETH_Ethereum'
     )
   {}
