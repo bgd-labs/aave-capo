1532e4ff77359fd79cb1a8d50add3551a528fd93
Harsh Pandey <harshsatishpandey@gmail.com>
2025-07-19 16:56:19 +0400

    chore: no fixed solc

--- src/contracts/lst-adapters/StSPriceCapAdapter.sol
+++ src/contracts/lst-adapters/StSPriceCapAdapter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
 import {PriceCapAdapterBase, IPriceCapAdapter} from '../PriceCapAdapterBase.sol';
 import {IStS} from '../../interfaces/IStS.sol';
