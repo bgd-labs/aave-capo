a50cf594030e15af217b4ebf32d3549fc70f2f63
Harsh Pandey <harshsatishpandey@gmail.com>
2025-08-18 12:26:43 +0400

    chore: bump libs and update usdt feed address

--- scripts/DeployInk.s.sol
+++ scripts/DeployInk.s.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.8.0;
 
 import {GovV3Helpers} from 'aave-helpers/GovV3Helpers.sol';
 import {InkScript} from 'solidity-utils/contracts/utils/ScriptUtils.sol';
-import {AaveV3Ink} from 'aave-address-book/AaveV3Ink.sol';
+import {AaveV3InkWhitelabel} from 'aave-address-book/AaveV3InkWhitelabel.sol';
 import {FixedPriceAdapter} from '../src/contracts/misc-adapters/FixedPriceAdapter.sol';
 import {PriceCapAdapterStable, IPriceCapAdapterStable, IChainlinkAggregator} from '../src/contracts/PriceCapAdapterStable.sol';
 
 library CapAdaptersCodeInk {
-  address public constant USDT_PRICE_FEED = 0x1273f29204fC102bD4620485B13cFE27a794fF32;
+  address public constant USDT_PRICE_FEED = 0x176A9536feaC0340de9f9811f5272E39E80b424f;
 
   function USDTAdapterCode() internal pure returns (bytes memory) {
     return
@@ -16,7 +16,7 @@ library CapAdaptersCodeInk {
         type(PriceCapAdapterStable).creationCode,
         abi.encode(
           IPriceCapAdapterStable.CapAdapterStableParams({
-            aclManager: AaveV3Ink.ACL_MANAGER,
+            aclManager: AaveV3InkWhitelabel.ACL_MANAGER,
             assetToUsdAggregator: IChainlinkAggregator(USDT_PRICE_FEED),
             adapterDescription: 'Capped USDT/USD',
             priceCap: int256(1.04 * 1e8)
