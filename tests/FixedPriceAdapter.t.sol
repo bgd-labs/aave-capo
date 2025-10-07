f3cd6a211b17a61cead0eab4115d29f64897a915
Harsh Pandey <harshsatishpandey@gmail.com>
2025-10-07 19:14:29 +0400

    fix: allow updating the fixed price feed

--- tests/FixedPriceAdapter.t.sol
+++ tests/FixedPriceAdapter.t.sol
@@ -2,22 +2,27 @@
 pragma solidity ^0.8.0;
 
 import {Test} from 'forge-std/Test.sol';
-import {FixedPriceAdapter} from '../src/contracts/misc-adapters/FixedPriceAdapter.sol';
+import {FixedPriceAdapter, IFixedPriceAdapter, IACLManager} from '../src/contracts/misc-adapters/FixedPriceAdapter.sol';
+import {ACLManagerMock} from './unit-tests/mocks/ACLManagerMock.sol';
 
 contract FixedPriceAdapterTest is Test {
   FixedPriceAdapter fixedPriceAdapter;
+  IACLManager aclManager;
+
+  address public constant POOL_ADMIN = address(50);
 
   int256 public constant FIXED_PRICE = 1e18;
   uint8 public constant DECIMALS = 8;
   string public DESCRIPTION = 'Fixed USDG / USD';
 
   function setUp() public {
-    fixedPriceAdapter = new FixedPriceAdapter(DECIMALS, FIXED_PRICE, DESCRIPTION);
+    aclManager = IACLManager(address(new ACLManagerMock(POOL_ADMIN, address(0))));
+    fixedPriceAdapter = new FixedPriceAdapter(address(aclManager), DECIMALS, FIXED_PRICE, DESCRIPTION);
   }
 
   function test_latestAnswer() external view {
     assertEq(fixedPriceAdapter.latestAnswer(), FIXED_PRICE);
-    assertEq(fixedPriceAdapter.PRICE(), FIXED_PRICE);
+    assertEq(fixedPriceAdapter.price(), FIXED_PRICE);
   }
 
   function test_decimals() external view {
@@ -27,6 +32,25 @@ contract FixedPriceAdapterTest is Test {
 
   function test_description() external view {
     assertEq(fixedPriceAdapter.description(), DESCRIPTION);
+  }
+
+  function test_setPrice(int256 newFixedPrice) external {
+    int256 initialPrice = fixedPriceAdapter.price();
+
+    vm.expectEmit(true, true, true, true);
+    emit IFixedPriceAdapter.FixedPriceUpdated(initialPrice, newFixedPrice);
+
+    vm.prank(POOL_ADMIN);
+    fixedPriceAdapter.setPrice(newFixedPrice);
+    assertEq(fixedPriceAdapter.price(), newFixedPrice);
+  }
+
+  function test_setPrice_callerNotPoolAdmin(address caller) external {
+    vm.assume(caller != POOL_ADMIN);
+    vm.prank(caller);
+
+    vm.expectRevert(IFixedPriceAdapter.CallerIsNotPoolAdmin.selector);
+    fixedPriceAdapter.setPrice(1);
   }
 }
 
